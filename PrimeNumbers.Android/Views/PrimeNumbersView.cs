c0456caba1a4920afd819f04f9ba74a733dcae16
ANDRES FELIPE FRANCO GUZMAN <andresfranco164045@correo.itm.edu.co>
2019-11-23 11:25:22 -0500

    Finish!

---
+++ PrimeNumbers.Android/Views/PrimeNumbersView.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using PrimeNumbers.Core.ViewModels;
+
+namespace PrimeNumbers.Android.Views
+{
+    [Activity(Label = "@string/app_name")]
+    public class PrimeNumbersView : MvxActivity<PrimeNumbersViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            this.SetContentView(Resource.Layout.PrimeNumbersPage);
+        }
+    }
+}
